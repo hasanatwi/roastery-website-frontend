463510ba7736faa384334cb41351ec24f8c6ffeb
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2025-12-09 00:19:08 +0200

    Update Category.jsx

--- Category.jsx
+++ Category.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Item from "./Item";
 
 function Category(props) {
   const matchesCategory =
@@ -12,70 +11,43 @@ function Category(props) {
     props.title.startsWith("Chinese") ||
     props.title.startsWith("Seeds") ||
     props.title.startsWith("Dried");
+
   return (
     <div
       className="Category"
       onClick={props.onClick}
-      style={{
-        cursor: "pointer",
-      }}
+      style={{ cursor: "pointer" }}
     >
       {!matchesCategory && props.title === "" && (
         <Link to={`/`}>
           <img
             src={props.image}
             alt={props.title}
-            style={{
-              width: "270px",
-              height: "200px",
-              objectFit: "cover",
-            }}
+            style={{ width: "270px", height: "200px", objectFit: "cover" }}
           />
         </Link>
       )}
+
       {matchesCategory && props.title !== "" && (
         <Link to={`/displaySpecificProducts/${props.title}`}>
           <img
             src={props.image}
             alt={props.title}
-            style={{
-              width: "270px",
-              height: "200px",
-              objectFit: "cover",
-            }}
+            style={{ width: "270px", height: "200px", objectFit: "cover" }}
           />
         </Link>
       )}
+
       {!matchesCategory && props.title !== "" && (
-        <Link
-          to={`/item/${props.name_of_the_category}/${props.title}`}
-        >
+        <Link to={`/item/${props.name_of_the_category}/${props.title}`}>
           <img
             src={props.image}
             alt={props.title}
-            style={{
-              width: "270px",
-              height: "200px",
-              objectFit: "cover",
-            }}
+            style={{ width: "270px", height: "200px", objectFit: "cover" }}
           />
         </Link>
       )}
-      {!matchesCategory && props.title === "coffee" && (
-        <Link
-          to={`/item/${props.name_of_the_category}/${props.title}`}
-        >
-          <img
-            src={props.image}
-            alt={props.title}
-            style={{
-              width: "270px",
-              height: "200px",
-              objectFit: "cover",
-            }}
-          />
-        </Link>
-      )}
+
       <p>{props.title}</p>
     </div>
   );

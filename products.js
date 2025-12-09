0b9bd59d8cd1c8d19a4b49b4957a6717499ace75
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2025-12-09 11:09:19 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    id: 1,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/coffee%20beans.jpeg",
+    title: "Coffee",
+  },
+  {
+    id: 2,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/Roasted%20Nuts.jpeg",
+    title: "Roasted Nuts",
+  },
+  {
+    id: 3,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/raw%20nuts.jpeg",
+    title: "Raw Nuts",
+  },
+  {
+    id: 4,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/dates.jpeg",
+    title: "Dates",
+  },
+  {
+    id: 5,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/sugar%20dried%20fruits.jpeg",
+    title: "Dried Fruits",
+  },
+  {
+    id: 6,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/candies%20and%20jellies.jpeg",
+    title: "Candies and Jellies",
+  },
+  {
+    id: 7,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/chocolate%20gifts.jpeg",
+    title: "Chocolate Gifts",
+  },
+  {
+    id: 8,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/chinese%20mixed%20nuts.jpeg",
+    title: "Chinese",
+  },
+  {
+    id: 9,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/seeds.jpeg",
+    title: "Seeds",
+  },
+  {
+    id: 10,
+    image:
+      "https://lkxdeoexdxrizzkblvac.supabase.co/storage/v1/object/public/product-images/Golden%20Nuts.jpeg",
+    title: "",
+  },
+];
+
+export default products;

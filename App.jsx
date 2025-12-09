bdef3ee411c14d089012d7de3d239d7111b8979c
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2025-12-09 10:32:31 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import Note from "./components/Note";
+import DisplayProducts from "./components/DisplayProducts";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import DisplaySpecificProducts from "./components/displaySpecificProducts";
+import Item from "./components/Item";
+import UserProducts from "./components/UserProducts";
+
+function App() {
+  const [isValid, setIsValid] = useState(false);
+  const [nameOfTheUser, setNameOfTheUser] = useState("");
+  const [email, setEmail] = useState("");
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={<Note isValid2={isValid} nameOfTheUser={nameOfTheUser} />}
+          />
+          <Route
+            path="/shop"
+            element={
+              <DisplayProducts
+                isValid2={isValid}
+                nameOfTheUser={nameOfTheUser}
+              />
+            }
+          />
+          <Route
+            path="/sign_up"
+            element={
+              <SignUp
+                isValid2={isValid}
+                setIsValid2={setIsValid}
+                setNameOfTheUser2={setNameOfTheUser}
+                setEmail2={setEmail}
+              />
+            }
+          />
+          <Route
+            path="/sign_in"
+            element={
+              <SignIn
+                isValid2={isValid}
+                setIsValid2={setIsValid}
+                setNameOfTheUser2={setNameOfTheUser}
+                setEmail2={setEmail}
+              />
+            }
+          />
+          <Route
+            path="/displaySpecificProducts/:title"
+            element={
+              <DisplaySpecificProducts
+                isValid2={isValid}
+                nameOfTheUser={nameOfTheUser}
+              />
+            }
+          />
+          <Route
+            path="/item/:name_of_the_category/:title"
+            element={
+              <Item
+                isValid2={isValid}
+                nameOfTheUser={nameOfTheUser}
+                email2={email}
+              />
+            }
+          />
+
+          <Route
+            path="/cart"
+            element={
+              isValid ? (
+                <UserProducts email={email} nameOfTheUser={nameOfTheUser} />
+              ) : (
+                <Navigate to="/sign_in" replace />
+              )
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

44012ebffe2ef38185d87bfb475ee8320ce0eb89
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2026-08-22 12:12:27 +0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -63,6 +63,7 @@ function App() {
               <DisplaySpecificProducts
                 isValid2={isValid}
                 nameOfTheUser={nameOfTheUser}
+                email={email}
               />
             }
           />

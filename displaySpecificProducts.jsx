acf4259b58e350817e47096b9aa9367080a53983
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2026-08-22 12:03:46 +0300

    Update displaySpecificProducts.jsx

--- displaySpecificProducts.jsx
+++ displaySpecificProducts.jsx
@@ -4,12 +4,12 @@ import { useParams } from "react-router-dom";
 import Header from "./Header";
 import Category from "./Category";
 function DisplaySpecificProducts({isValid, nameOfTheUser, email}){
+    const {title}=useParams();
+    console.log("The title is: "+title);
     console.log("Hello world");
     console.log("I am in displaySpecificProducts The value of isValid is: "+isValid);
     console.log("The value of nameOfTheUser is: "+nameOfTheUser);
     console.log("The value of email is: "+email);
-    const {title}=useParams();
-    console.log("The title is: "+title);
     const [products, setProducts]=useState([]);
     const [loading, setLoading]=useState(false);
     useEffect(()=>{
